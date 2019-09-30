1be1fe69b4131b7170e9d90ff908bec2b0b948e7
Montaser Zaghmout <mont0004@edu.easj.dk>
2019-09-30 13:41:20 +0200

    fourth commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
 const autoprefixer = require("autoprefixer");
 const cssnaano = require("cssnano");
 const sass = require("gulp-sass");
@@ -9,8 +10,10 @@ const rename = require("gulp-rename");
 function styles() {
     return(
         gulp.src("css/*.scss")
+            .pipe(sourcemaps.init())
             .pipe(sass())
             .pipe(postcss([autoprefixer({grid:true}),cssnaano()]))
+            .pipe(sourcemaps.write("."))
             .pipe(gulp.dest("css"))
 
     );
@@ -19,10 +22,12 @@ function styles() {
 function js() {
     return(
         gulp.src(["js/*.js","!js/*min.js"])
+            .pipe(sourcemaps.init())
             .pipe(terser())
             .pipe(rename({
                 suffix:".min"
             }))
+            .pipe(sourcemaps.write("."))
             .pipe(gulp.dest("js"))
     );
 }
