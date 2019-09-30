39958d2653f059a7bc13db5773763502b39ba8f6
Montaser Zaghmout <mont0004@edu.easj.dk>
2019-09-30 12:17:10 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+
+function styles() {
+    return(
+        gulp.src("css/*.scss")
+            .pipe(sass())
+            .pipe(gulp.dest("css"))
+
+    );
+}
+
+function watch() {
+    gulp.watch("css/*.scss", styles);
+    
+}
+
+exports.styles = styles;
+exports.watch = watch;
